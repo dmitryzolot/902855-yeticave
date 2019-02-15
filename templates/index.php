ee6858242ff45e083819b3e2e5f4fc334937bf79
dmitryzolot <44970728+dmitryzolot@users.noreply.github.com>
2019-02-15 14:08:09 +0200

    MySQL из PHP
    подключили списки категорий и лотов из БД

--- templates/index.php
+++ templates/index.php
@@ -8,7 +8,7 @@
 			<?php foreach ($categories as $key => $value): ?>
 			<li class="promo__item promo__item--boards">	
 				<a class="promo__link" href="pages/all-lots.html">
-					<?= $value ?>
+					<?= $value['name'] ?>
 				</a>
 			</li>
 			<?php endforeach; ?>
@@ -25,15 +25,15 @@
 			<?php foreach ($adverts as $key => $item): ?>
 			<li class="lots__item lot">
 				<div class="lot__image">
-                    <img src="<?=$item['url']; ?>" width="350" height="260" alt="">
+                    <img src="<?=$item['picture']; ?>" width="350" height="260" alt="">
                 </div>
 				<div class="lot__info">
-					<span class="lot__category"><?=$item['category']; ?></span>
+					<span class="lot__category"><?=$item['category_name']; ?></span>
 					<h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($item['name']); ?></a></h3>
 					<div class="lot__state">
 						<div class="lot__rate">
 							<span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= number_format(ceil(htmlspecialchars($item['price'])),0, '.', ' ') ?> <b class="rub">р</b></span>
+                            <span class="lot__cost"><?= number_format(ceil(htmlspecialchars($item['initial_price'])),0, '.', ' ') ?> <b class="rub">р</b></span>
 						</div>
 						<div class="lot__timer timer">
                             <?=$hm_tomidnight; ?>
